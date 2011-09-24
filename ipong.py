f9dc52fe6a5e11d1fd1e1eff333f0bbe7df17cc1
markosamuli <contact@markosamuli.com>
2011-09-24 16:03:24 +0300

    Updated server

--- ipong.py
+++ ipong.py
@@ -1,3 +1,11 @@
+#!/usr/bin/env python
+
+import argparse
+import re
+
+from gevent import pywsgi
+from gevent.pool import Pool
+
 def application(env, start_response):
     
     if env['PATH_INFO'] == '/favicon.ico':
@@ -5,10 +13,33 @@ def application(env, start_response):
         return ['']
     
     if env['PATH_INFO'] == '/':
-        output = env['REMOTE_ADDR']
-        start_response('200 OK', [('Content-type', 'text/plain'), ('Content-length', str(len(output)))])
+        if 'HTTP_X_REAL_IP' in env:
+            output = env['HTTP_X_REAL_IP']
+        else:
+            output = env['REMOTE_ADDR']
+        start_response('200 OK', [('Content-type', 'text/plain')])
         return [output]
         
     start_response('404 Not Found', [('Content-type', 'text/plain')])
     return ['Ping pong!']
     
+def valid_address(string):
+    if re.match('^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$', string):
+        return string    
+    msg = "%s is not valid address" % string
+    raise argparse.ArgumentTypeError(msg)
+    
+def main():
+    parser = argparse.ArgumentParser(description='ipong server')
+    parser.add_argument('--address', type=valid_address, default='0.0.0.0')    
+    parser.add_argument('--port', type=int, default=8088)
+    args = parser.parse_args()
+    address = args.address
+    port = args.port
+    print "ipong -- starting server at http://" + address + ":" + str(port)
+    #pool = Pool(10000) # do not accept more than 10000 connections
+    #pywsgi.WSGIServer((host, port), application, spawn=pool).serve_forever()
+    pywsgi.WSGIServer((address, port), application).serve_forever()
+
+if __name__ == '__main__':
+    main()
